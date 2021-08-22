a4dd46ef36f75dbe5d4c83c8e4dde3f3f42937f0
Muggle Lizzie <mlizzie@et-f4.kzn.21-school.ru>
2021-08-22 14:15:11 +0300

    minishell

---
+++ env/env_utils4.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_utils4.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmorgana <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/20 10:45:04 by lmorgana          #+#    #+#             */
+/*   Updated: 2021/08/20 10:45:06 by lmorgana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header.h"
+
+int	ft_increase_shlvl(char **shlvl_a)
+{
+	int	shlvl_i;
+
+	shlvl_i = ft_atoi(*shlvl_a);
+	if (shlvl_i > 0)
+	{
+		shlvl_i++;
+		free(*shlvl_a);
+		*shlvl_a = ft_itoa(shlvl_i);
+		return (1);
+	}
+	return (0);
+}
+
+char	*ft_my_getenv(char *name, t_envs *env)
+{
+	t_envs	*list;
+
+	list = ft_get_by_name(name, env);
+	if (list)
+	{
+		return (list->value);
+	}
+	return (NULL);
+}
